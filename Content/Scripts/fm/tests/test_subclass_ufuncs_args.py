f3c2c59d02f1617a0c65d547d6df920d84992921
dave <dave.brueck@gmail.com>
2018-11-22 16:24:12 -0700

    progress on func param tests

--- Content/Scripts/fm/tests/test_subclass_ufuncs_args.py
+++ Content/Scripts/fm/tests/test_subclass_ufuncs_args.py
@@ -5,9 +5,12 @@ from fm.common import *
 import fm
 bridge = fm.subclassing.bridge
 ufunction = fm.subclassing.ufunction
+from unreal_engine import FVector, FRotator, FTransform, FColor, FLinearColor
+from unreal_engine.structs import BoxSphereBounds as BSB
+from unreal_engine.enums import EControllerHand
 
 # basic types: bool, int, float, string
-if 1:
+if 0:
     log('-- TEST 0 --')
     class T0Child(bridge.CChild):
         @ufunction
@@ -23,9 +26,47 @@ if 1:
     log('d')
 
 # core builtin structs: FVector, FRotator, FTransform, FColor, FLinearColor
-# obj: obj of a certain type, class of a certain type, obj that implements an interface
+if 0:
+    log('-- TEST 1 --')
+    class T1Child(bridge.CChild):
+        @ufunction
+        def MyFunc(self, v:FVector, r:FRotator, t:FTransform, c:FColor, l:FLinearColor):
+            log('v:', v)
+            log('r:', r)
+            log('t:', t)
+            log('c:', c)
+            log('l:', l)
+
+    log('a')
+    c = Spawn(T1Child)
+    log('b')
+    t = FTransform()
+    t.translation = FVector(2.1, 3.2, 4.3)
+    t.rotation = FRotator(4.1, 4.2, 4.3)
+    t.scale = FVector(5.5, 6.6, 7.7)
+    c.MyFunc(FVector(11, 12, 13), FRotator(14, 15, 16), t, FColor(50, 51, 52, 53), FLinearColor(0.1, 0.2, 0.3, 0.4))
+    log('c')
+    c.get_py_proxy().MyFunc(FVector(11, 12, 13), FRotator(14, 15, 16), t, FColor(50, 51, 52, 53), FLinearColor(0.1, 0.2, 0.3, 0.4))
+    log('d')
+
+# enums
+if 1:
+    log('-- TEST 2 --')
+    class T2(bridge.CChild):
+        @ufunction
+        def MyFunc(self, e:EControllerHand):
+            log('e:', e)
+
+    log('a')
+    c = Spawn(T2)
+    log('b')
+    c.MyFunc(EControllerHand.Right)
+    log('c')
+    c.get_py_proxy().MyFunc(EControllerHand.Right)
+    log('d')
+
 # builtin UStructs
 # custom UStructs
-# enums
+# obj: obj of a certain type, class of a certain type, obj that implements an interface
 # array, map, set args
 
